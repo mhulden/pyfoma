b1f67289415216fa4e4cff0d387daaa9ae82bab5
michaelpginn <milespergallon271@gmail.com>
2022-09-02 00:38:49 -0600

    Arrange project for pip

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+    name = "pyfoma",
+    version = "0.0.7",
+    author = "Mans Hulden",
+    author_email = "mans.hulden@colorado.edu",
+    description = "Python Finite-State Toolkit",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/mhulden/pyfoma",
+    project_urls = {
+        "Bug Tracker": "https://github.com/mhulden/pyfoma/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    py_modules = ["pyfoma"],
+    python_requires = ">=3.6",
+    install_requires = [
+        "graphviz", "IPython"
+    ]
+)
